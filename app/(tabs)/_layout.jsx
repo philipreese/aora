7fe76175f6fd6834ae36cd377c65d45709c4a771
Philip Reese <philipreese@mail.com>
2024-10-07 23:45:37 -0400

    create home page

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image } from "react-native";
 import { Tabs, Redirect } from "expo-router";
 import { icons } from "../../constants";
+import { StatusBar } from "expo-status-bar";
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
@@ -98,6 +99,8 @@ const TabsLayout = () => {
           }}
         />
       </Tabs>
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
 };
---
+++ components/Trending.jsx
@@ -0,0 +1,15 @@
+import { Text, FlatList } from "react-native";
+
+const Trending = ({ posts }) => {
+  return (
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <Text className="text-3xl text-white">{item.id}</Text>
+      )}
+      horizontal
+    />
+  );
+};
+export default Trending;
